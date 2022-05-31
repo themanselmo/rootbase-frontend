43464615c122bb7e4ca1925e893a6932b9eff9c9
natehains <nathan.hains@modelb.com>
2022-05-31 09:18:50 -0400

    Simple landing page animation

---
+++ src/components/atoms/icons/PlantPot.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const PlantPot = ({ height = "20px", width = "20px" }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={height}
+      width={width}
+      viewBox="0 0 612 612"
+    >
+      <g filter="url(#filter0_d_3_2)">
+        <g filter="url(#filter1_d_3_2)">
+          <path d="M68.207 202.64c0-12.337 10-22.338 22.338-22.338l425.165-.001c12.337 0 22.338 10 22.338 22.338v23.31c0 12.336-10 22.337-22.338 22.337h-15.219L468.58 466.89c-1.602 10.974-11.012 19.11-22.103 19.11H159.783c-11.091 0-20.501-8.136-22.103-19.11l-31.919-218.605H90.545c-12.337 0-22.338-9.999-22.338-22.337V202.64z"></path>
+          <path d="M106.256 248.213l-.062-.428H90.545c-12.06 0-21.838-9.775-21.838-21.837V202.64c0-12.061 9.777-21.838 21.838-21.838l425.165-.001c12.061 0 21.838 9.777 21.838 21.838v23.31c0 12.06-9.776 21.837-21.838 21.837h-15.652l-.062.428-31.911 218.604c-1.566 10.729-10.766 18.682-21.608 18.682H159.783c-10.843 0-20.042-7.953-21.609-18.682l-31.918-218.605z"></path>
+        </g>
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_3_2"
+          width="477.841"
+          height="313.699"
+          x="64.207"
+          y="180.301"
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_3_2"
+            result="shape"
+          ></feBlend>
+        </filter>
+        <filter
+          id="filter1_d_3_2"
+          width="477.841"
+          height="313.699"
+          x="64.207"
+          y="180.301"
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_3_2"
+            result="shape"
+          ></feBlend>
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default PlantPot;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import logo from "../assets/rootbase-logo.png";
-
+import styled from "styled-components";
 // component imports
 import { Stack } from "@mui/material";
 import Button from "@mui/material/Button";
@@ -12,6 +12,10 @@ import {
   registerOrg,
   resetOrg,
 } from "../features/authOrg/authOrgSlice";
+import PlantPot from "../components/atoms/icons/PlantPot";
+import Seedling from "../components/atoms/icons/Seedling";
+import Tree from "../components/atoms/icons/Tree";
+import Circle from "../components/atoms/icons/Circle";
 
 const LoginPage = () => {
   const [loggingIn, setLoggingIn] = useState(true);
@@ -30,6 +34,8 @@ const LoginPage = () => {
   );
 
   useEffect(() => {
+    seedToTree();
+
     if (isError) {
       console.log(message);
     }
@@ -60,9 +66,66 @@ const LoginPage = () => {
     loggingIn ? dispatch(loginOrg(userData)) : dispatch(registerOrg(userData));
   };
 
+  const [flip, setFlip] = useState(true);
+  const [hidden, setHidden] = useState(true);
+  const [hidden2, setHidden2] = useState(false);
+
+  const seedToTree = () => {
+    setFlip((prevState) => !prevState);
+
+    setHidden((prevState) => !prevState);
+    setTimeout(() => {
+      setHidden2((prevState) => !prevState);
+    }, 1000);
+  };
+
   return (
     <div id="login-page">
-      <img src={logo} id="login-logo" />
+      <Div>
+        <span className={`moon-sun ${flip ? "daylight-animated" : "nightlight-animated"}`}>
+          <Circle height="100px" width="100px" />
+        </span>
+       
+        
+
+        <section className="upper-animation">
+          <span
+            className={`${
+              flip ? "tree popdown-animated" : "tree popup-animated"
+            }  ${!hidden ? "hidden" : ""}`}
+          >
+            <Tree width="150px" height="150px" />
+          </span>
+          <span
+            className={`${!flip ? "tree popup-animated" : "tree"} ${
+              hidden2 ? "hidden" : ""
+            }`}
+          >
+            <Tree width="150px" height="150px" />
+          </span>
+          <span
+            className={`${
+              !flip ? "seedling popdown-animated" : "seedling popup-animated"
+            }  ${hidden ? "hidden" : ""}`}
+          >
+            <Seedling width="150px" height="150px" />
+          </span>
+          <span
+            className={`${flip ? "seedling popup-animated" : "seedling"} ${
+              !hidden2 ? "hidden" : ""
+            }`}
+          >
+            <Seedling width="150px" height="150px" />
+          </span>
+        </section>
+
+        <section className="lower-animation">
+          <span className="plant-pot">
+            <PlantPot width="250px" height="250px" />
+          </span>
+        </section>
+      </Div>
+
       {loggingIn ? (
         <Stack>
           <Input
@@ -84,6 +147,7 @@ const LoginPage = () => {
           <Button
             color="secondary"
             onClick={() => {
+              seedToTree();
               setLoggingIn(false);
             }}
           >
@@ -112,6 +176,7 @@ const LoginPage = () => {
           <Button
             color="secondary"
             onClick={() => {
+              seedToTree();
               setLoggingIn(true);
             }}
           >
@@ -125,3 +190,88 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 400px;
+  width: 400px;
+  .upper-animation{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+  }
+  .lower-animation{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+  }
+  .tree {
+    fill: #598f14;
+    position: absolute;
+    bottom: -13px
+  }
+  .seedling {
+    fill: #598f14;
+    position: absolute;
+    bottom: -22px;
+  }
+  .plant-pot {
+    fill: #936647;
+    position: absolute;
+    bottom: 20px;
+  }
+  .hidden{
+    display: none;
+  }
+  .moon-sun {
+    position: absolute;
+    transition: 1s;
+    animation: sun-to-moon 1s linear;
+    transform: rotate(0deg) translateX(200px) rotate(0deg)
+  }
+
+  .daylight-animated {
+    fill: yellow;
+    animation: sun-to-moon 1s linear;
+  }
+
+  .nightlight-animated{
+    fill: white;
+    animation: sun-to-moon 1s linear reverse;
+  }
+  
+  .popup-animated {
+    animation-timing-function: cubic-bezier(0.280, 0.840, 0.420, 1);
+    animation: cartoon-slide 1s ease;
+  }
+
+  .popdown-animated {
+    animation-timing-function: cubic-bezier(0.280, 0.840, 0.420, 1);
+    animation: cartoon-slide 1s ease reverse
+  }
+
+  @keyframes cartoon-slide {
+        0%   { transform: scale(1,1)      translateY(0); }
+        10%  { transform: scale(1.1,.9)   translateY(0); }
+        30%  { transform: scale(.9,1.1)   translateY(-50px); }
+        50%  { transform: scale(1.05,.95) translateY(100); }
+        57%  { transform: scale(1,1)      translateY(200px); }
+        64%  { transform: scale(1,1)      translateY(300); }
+        100% { transform: scale(1,1)      translateY(300px); }
+    }
+
+  @keyframes sun-to-moon {
+    from { fill: yellow;}
+    to  { fill: white;}
+  }
+  }
+`;
