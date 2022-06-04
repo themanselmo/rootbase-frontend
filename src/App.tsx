068f259d2e1f5aa2e13c038a998d1d3142e3013c
natehains <nathan.hains@modelb.com>
2022-06-04 17:40:41 -0400

    TS files to TSX to prevent compiler errors, pre compile

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
